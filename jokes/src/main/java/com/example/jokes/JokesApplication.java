bf9d617b5196e6dd81924819ff57c8f54947e9ce
umang bhanushali <110281747+BhanushaliUmang@users.noreply.github.com>
2023-05-18 15:30:48 +0530

    Add files via upload

---
+++ jokes/src/main/java/com/example/jokes/JokesApplication.java
@@ -0,0 +1,51 @@
+package com.example.jokes;
+
+import org.json.JSONObject;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@SpringBootApplication
+public class JokesApplication {
+
+	public static void main(String[] args) throws Exception {
+
+		URL getUrl = new URL("https://api.chucknorris.io/jokes/random");
+		HttpURLConnection connection = (HttpURLConnection) getUrl.openConnection();
+		connection.setRequestMethod("GET");
+
+
+		int responseCode = connection.getResponseCode();
+
+		if (responseCode == 200) {
+
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			StringBuilder jsonResponseData = new StringBuilder();
+			String readLine = null;
+
+
+			while ((readLine = in.readLine()) != null) {
+				jsonResponseData.append(readLine);
+			}
+
+			in.close();
+			JSONObject masterData = new JSONObject(jsonResponseData.toString());
+
+			System.out.println("Jokes => " + masterData.get("value"));
+			// System.out.println(masterData);
+			// System.out.println("Jokes => " + masterData.get("created_at"));
+			// System.out.println("Jokes => " + masterData.get("id"));
+			
+		} 
+		else {
+			System.out.println("This is not valid URL- " + responseCode);
+		}
+
+
+	}
+
+}
